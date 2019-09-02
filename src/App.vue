874fd0058cd4b351850382bd21e3d620670ef373
William Cameron <wp.cameron@gmail.com>
2019-09-02 22:42:29 +0100

    Add navbar and some bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <NavBar></NavBar>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +8,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, NavBar
   }
 }
 </script>
@@ -23,6 +26,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import jQuery from 'jquery'
+window.$ = window.jQuery = jQuery;
+import 'popper.js'
+import 'bootstrap'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
