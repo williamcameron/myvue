382b0fe9db93727a73a7a581987dd617d33dbf15
William Cameron <wp.cameron@gmail.com>
2019-09-03 22:24:02 +0100

    router

--- src/main.js
+++ src/main.js
@@ -5,9 +5,22 @@ import jQuery from 'jquery'
 window.$ = window.jQuery = jQuery;
 import 'popper.js'
 import 'bootstrap'
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
 
+import Home from './components/Home.vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/hello', component: HelloWorld }
+];
+const router = new VueRouter({
+    routes
+});
+Vue.use(VueRouter);
 new Vue({
     render: h => h(App),
+    router
 }).$mount('#app')
